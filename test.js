df76b0613398fec1c66b8f2fca261c1670ffd2f5
Imamul Kadir <imamulkadiroyan@gmail.com>
2021-05-31 22:16:34 +0600

    Updated

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+// Square
 function square(x) {
     var cal = x*x;
     return cal;
@@ -6,4 +7,20 @@ function square(x) {
 var ans = square(5);
 
 print(ans);
-console.log(ans);
+
+// String Concatenet
+
+function stringCon(a,b) {
+    var concate = a + b;
+    return concate;
+}
+
+var result = stringCon('Hello ', 'beautiful!');
+
+print(result);
+
+// Get image width
+
+var img = new SimpleImage("smallpanda.png");
+print(img.getWidth());
+print(img);
